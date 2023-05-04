0a324114705b0cb514637d0b7e6d3631fe66a2b9
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-05-04 07:50:25 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ const DefaultData = require('./defaultdata.js');
 const cors = require('cors')
 const router = require('./routes/router')
 
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
+app.use(cors(corsOptions));
 app.use(express.json()); 
 app.use(cookieParser(""))
 app.use(router);
