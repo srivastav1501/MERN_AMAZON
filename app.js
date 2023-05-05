22e4f35c13d406ec5b9a5838f7b5f89815b38ab1
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-05-05 16:41:46 +0530

    changes

--- app.js
+++ app.js
@@ -11,13 +11,9 @@ const DefaultData = require('./defaultdata.js');
 const cors = require('cors')
 const router = require('./routes/router')
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,          
-    optionSuccessStatus:200,
- }
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json()); 
 app.use(cookieParser(""))
 app.use(router);
