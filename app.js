a8f8fa92ae316a3f8cc9e680eb68885674676350
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-05-05 22:23:51 +0530

    first commi

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const DefaultData = require('./defaultdata.js');
 const router = require('./routes/router')
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Credentials", true);
     res.header(
       "Access-Control-Allow-Headers",
