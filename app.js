ff3b8d561272e83238530c356d97085c3fba523b
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-05-05 20:31:07 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ const Products = require('./modals/productsSchema')
 const DefaultData = require('./defaultdata.js');
 const router = require('./routes/router')
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.header('Origin'));
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 
 app.use(cors());
 app.use(express.json()); 
