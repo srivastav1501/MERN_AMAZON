c366618419dd60ed64649fe404e9827faa6ceaee
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-05-06 12:07:57 +0530

    cahng

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const DefaultData = require('./defaultdata.js');
 const router = require('./routes/router')
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://amazon-frontend-a3y5.onrender.com");
     res.header("Access-Control-Allow-Credentials", true);
     res.header(
       "Access-Control-Allow-Headers",
