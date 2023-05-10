808006bff31c049ceb87d5bb4b4d8aec598e99b2
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-05-10 16:40:26 +0530

    modified

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-// const mongoose = require('mongoose')
 require('./db/conn');
 const cookieParser = require("cookie-parser")
 const cors = require('cors');
